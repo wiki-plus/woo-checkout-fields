3c22a6c16ad2ae9b7420cfd2df415e1530189c0a
hossein <hossein.soleimany445@gmail.com>
2024-10-04 19:57:06 +0330

    change functions name

--- code.php
+++ code.php
@@ -1,7 +1,6 @@
 <?php
-
-// حذف فیلد کمپانی ، ایمیل، کدپستی، آدرس 2
-function wc_remove_checkout_fields( $fields ) {
+// حذف فیلد کمپانی، ایمیل، کدپستی، آدرس 2
+function wikiplus_remove_specific_checkout_fields( $fields ) {
 
     // Billing fields
     unset( $fields['billing']['billing_company'] );
@@ -10,17 +9,17 @@ function wc_remove_checkout_fields( $fields ) {
     unset( $fields['billing']['billing_postcode'] );
 
     // Shipping fields
-     unset( $fields['shipping']['shipping_email'] );
+    unset( $fields['shipping']['shipping_email'] );
     unset( $fields['shipping']['shipping_company'] );
     unset( $fields['shipping']['shipping_address_2'] );
     unset( $fields['shipping']['shipping_postcode'] );
-  
+
     return $fields;
 }
-add_filter( 'woocommerce_checkout_fields', 'wc_remove_checkout_fields' );
+add_filter( 'woocommerce_checkout_fields', 'wikiplus_remove_specific_checkout_fields' );
 
 // یکی کردن فیلد نام و نام خانوادگی و غیرضروری کردن آدرس و تغییر متن پیشفرض آدرس 
-function wc_remove_checkout_fields($fields)
+function wikiplus_customize_name_address_fields($fields)
 {
 
     // Billing fields
@@ -56,21 +55,20 @@ function wc_remove_checkout_fields($fields)
 
     return $fields;
 }
-add_filter('woocommerce_checkout_fields', 'wc_remove_checkout_fields');
+add_filter('woocommerce_checkout_fields', 'wikiplus_customize_name_address_fields');
 
 // تغییر ترتیب نمایش فیلدها بصورت دلخواه
-function wc_remove_checkout_fields( $fields ) {
+function wikiplus_reorder_checkout_fields( $fields ) {
 
-  $fields['billing']['billing_first_name']['priority'] = 10;
-  $fields['billing']['billing_phone']['priority'] = 20;
-  $fields['billing']['billing_email']['priority'] = 30;
-  $fields['billing']['billing_country']['priority'] = 40;
-  $fields['billing']['billing_state']['priority'] = 50;
-  $fields['billing']['billing_city']['priority'] = 60;
-  $fields['billing']['billing_address_1']['priority'] = 70;
-  $fields['billing']['billing_postcode']['priority'] = 80;
-  
-  return $fields;
-  
+    $fields['billing']['billing_first_name']['priority'] = 10;
+    $fields['billing']['billing_phone']['priority'] = 20;
+    $fields['billing']['billing_email']['priority'] = 30;
+    $fields['billing']['billing_country']['priority'] = 40;
+    $fields['billing']['billing_state']['priority'] = 50;
+    $fields['billing']['billing_city']['priority'] = 60;
+    $fields['billing']['billing_address_1']['priority'] = 70;
+    $fields['billing']['billing_postcode']['priority'] = 80;
+
+    return $fields;
 }
-add_filter( 'woocommerce_checkout_fields', 'wc_remove_checkout_fields' );
+add_filter( 'woocommerce_checkout_fields', 'wikiplus_reorder_checkout_fields' );
